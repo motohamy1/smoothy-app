b428d6a8787cfc90bc144b29ad85464c1924e84f
motohamy1 <eltohamym660@gmail.com>
2025-08-28 18:03:36 +0300

    first part of Hero section has been done

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 
 import React from 'react'
 import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
 
 
 const App = () => {
     return (
         <main className="">
             <Navbar />
+            <Hero />
+            <div className="h-dvh bg-black"></div>
         </main>
 
     )
---
+++ src/components/Hero.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import {useGSAP} from "@gsap/react";
+import {SplitText} from "gsap/all";
+import {gsap} from "gsap";
+function Hero() {
+    useGSAP(()=>{
+        const heroSplit = new SplitText(".title", {type: "chars, words"})
+        const paragraph = new SplitText(".subtitle", {type: "lines"})
+
+        heroSplit.chars.forEach((char)=> char.classList.add('text-gradient'));
+        gsap.from(heroSplit.chars, {
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.06
+        });
+
+        gsap.from(paragraph.lines, {
+            opacity: 0,
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.06,
+            delay: 1
+        });
+
+        gsap.timeline({
+            scrollTrigger:{
+                trigger:'#hero',
+                start:'top top',
+                end:'bottom top',
+                scrub: true,
+            }
+        })
+            .to('.left-leaf', {y:200},0)
+            .to('.right-leaf', {y:-200},0)
+    },[]);
+
+
+    return (
+        <>
+            <section id='hero' className='noisy'>
+                <h1 className='title'>Smoothy</h1>
+
+                <img  src='/images/hero-right-leaf.png'
+                      alt='right-leaf'
+                      className='right-leaf'/>
+                <img  src='/images/hero-left-leaf.png'
+                      alt='left-leaf'
+                      className='left-leaf'/>
+
+                <div className='body'>
+                    <div className='content'>
+                        <div className='space-y-5 hidden md:block'>
+                            <p>Refresh. Cool. Classic</p>
+                            <p className='subtitle'>
+                                Smoothy , sip the spirit <br/> of the Summer.
+                            </p>
+                        </div>
+                        <div className='view-cocktails'>
+                            <p className='subtitle'>
+                                Inspired by the timeless flavors of the Nile Valley,
+                                our refreshing blends combine traditional Egyptian
+                                ingredients like hibiscus,mint, and citrus with
+                                modern mixology. Each sip captures the essence of
+                                Cairo's vibrant café culture
+                            </p>
+                            <a href='#cocktails'>View Cocktails</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Hero
